6d5074e99b93a32ebca710bdb44230302b829279
waikho <khowai@hotmail.com>
2023-11-28 23:18:16 +0800

    Update streamlit_ICI.py

--- streamlit_ICI.py
+++ streamlit_ICI.py
@@ -34,7 +34,7 @@ if columns_to_plot:
 
     # Create a new dataframe that contains only the selected columns
     df_to_plot = df2[columns_to_plot]
-
+    
     # Plot the selected columns
     #st.line_chart(df_to_plot, height=480)
 
@@ -46,10 +46,10 @@ if columns_to_plot:
     fig.update_layout(hovermode="x unified")
 
     st.plotly_chart(fig)
-
+    
 else:
     st.write("No columns selected.")
-
+    
 #Select month-range using st.select_slider to calculate MoM change
 #st.select_slider can select any data type, including str
 
@@ -66,15 +66,14 @@ MoM_start, MoM_end = st.select_slider('Select the month to show MoM change:',
                                       options=formatted_dates[-12:],
                                       value=(formatted_dates[-2],formatted_dates[-1]))
 
+
 idx_from = formatted_dates.index(MoM_start)
 idx_to = formatted_dates.index(MoM_end)
-
-#st.write('From index: ', idx_from)
-#st.write('To index: ', idx_to)
 
 df_diff = pd.DataFrame((df2.iloc[idx_to] - df2.iloc[idx_from]))
 
 st.write(df2.iloc[[idx_from, idx_to]])
+
 
 # Assuming df_diff is your DataFrame and it has been reset its index
 df_diff.reset_index(inplace=True)
@@ -82,31 +81,25 @@ df_diff.columns = ['index', 'value']
 
 fig, ax = plt.subplots()
 bars = ax.bar(df_diff['index'], df_diff['value'], 
-              color=['red' if x < 0 else 'blue' for x in df_diff['value']])  # color bars based on value
+              color=['red' if x < 0 else 'cyan' for x in df_diff['value']])  # color bars based on value
+
+# Change the plot background color
+ax.set_facecolor('black')
+
+
+# Change the figure background color
+fig.patch.set_facecolor('black')
+
+
+# Change the text color to dark gray
+ax.tick_params(colors='white')
+ax.xaxis.label.set_color('white')
+ax.yaxis.label.set_color('white')
 
 # annotate bars
 for bar in bars:
     y_val = bar.get_height()
     plt.text(bar.get_x() + bar.get_width()/2, y_val, round(y_val, 2), 
-             va='bottom' if y_val < 0 else 'top', color='black')
+             va='bottom' if y_val < 0 else 'top', color='white')
 
 st.pyplot(fig)
-
-#df_diff.reset_index(inplace=True)
-#st.write(df2.iloc[idx_to] - df2.iloc[idx_from])
-#st.bar_chart(df2.iloc[idx_to] - df2.iloc[idx_from])
-#st.bar_chart(df_diff)
-#st.write(df_diff)
-#st.bar_chart(df_diff, x=df_diff.index)
-#import matplotlib.pyplot as plt
-
-# Assuming df_diff is your DataFrame and it has been reset its index
-#df_diff.reset_index(inplace=True)
-#df_diff.columns = ['index', 'value']
-
-#fig, ax = plt.subplots()
-#ax.bar(df_diff['index'], df_diff['value'])
-#plt.xticks(rotation=90)  # Optional: only if x-labels are too long
-
-#st.pyplot(fig)
-
